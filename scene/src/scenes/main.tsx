3d1a97199d9c838b76f7de987a91ea9d1e4ce1bf
Giorgio <65674128+Gaffomdz@users.noreply.github.com>
2024-06-26 11:33:06 +0200

    fixed test

--- scene/src/scenes/main.tsx
+++ scene/src/scenes/main.tsx
@@ -6,7 +6,4 @@ let game: GameController | null = null
 export function main(): void {
   game = new GameController()
   game.realmController.switchRealm('antrom')
-  for (let i = 0; i < 6; i++) {
-    let pig = new Pig()
-  }
 }
